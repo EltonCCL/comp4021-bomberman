2525195382439c5c05ee8980e62f7f400f8c1375
ny423 <yuenhoting230401@gmail.com>
2023-05-08 12:22:09 +0800

    Modified UI for the end end page

--- Public/scripts/socket.js
+++ Public/scripts/socket.js
@@ -1,19 +1,19 @@
-const Socket = (function () {
+const Socket = (function() {
     // This stores the current Socket.IO socket
     let socket = null;
     let _playerID = null;
     // This function gets the socket from the module
-    const getSocket = function () {
+    const getSocket = function() {
         return socket;
     };
     // This function connects the server and initializes the socket
-    const connect = function () {
+    const connect = function() {
         socket = io();
         // Wait for the socket to connect successfully
 
         // Set up the movement event
         socket.on("move", (data) => {
-            setTimeout(function () {
+            setTimeout(function() {
                 game.move(data.playerID, data.movement, data.direction);
             }, 10);
         });
@@ -33,7 +33,7 @@ const Socket = (function () {
             }
 
             //when left & right buttons are pressed by player, the game start
-            if ($("#join-player1").html() != "Player 1 Click this to join" && $("#join-player2").html() != "Player 2 Click this to join") {
+            if ($("#join-player1").html() != "Player 1" && $("#join-player2").html() != "Player 2") {
                 $("#game-canvas").css('opacity', '1');
                 $("#waitingText").html("Game Start!");
                 UI.startGame();
@@ -47,8 +47,32 @@ const Socket = (function () {
             // disable all onclick listener
             $(document).unbind();
             getLeaderboard();
-            //CLONE leaderboard
-            $("#playerBorad").clone().prependTo("#game-stat");
+            // // CLONE leaderboard
+            // $("#score-board").clone().prependTo("#end-game-ranking");
+            // Load leaderboard again instead of cloning
+            socket.on("get leaderboard", (leaderboard) => {
+                var leaderboardArray = Object.entries(leaderboard);
+                // sorting ranks
+                leaderboardArray.sort(function(a, b) {
+                    return b[1] - a[1];
+                });
+
+                // use getElement to get object instead of jquery
+                var tableBody = document.getElementById("end-game-table-body");
+
+                // loop & get all data
+                leaderboardArray.forEach(function(data, index) {
+                    var row = tableBody.insertRow(); // Create a new row
+                    // Create cells and populate with data
+                    var rankingCell = row.insertCell();
+                    rankingCell.textContent = index + 1;
+                    var nameCell = row.insertCell();
+                    nameCell.textContent = data[0];
+                    var scoreCell = row.insertCell();
+                    scoreCell.textContent = data[1];
+                });
+            });
+            $("#playerBorad").clone().prependTo("#game-stat-text");
             UI.endGame(); // TODO: pass in arguments for showing the game stat after end game
         });
 
@@ -60,7 +84,7 @@ const Socket = (function () {
         socket.on("get leaderboard", (leaderboard) => {
             var leaderboardArray = Object.entries(leaderboard);
             // sorting ranks
-            leaderboardArray.sort(function (a, b) {
+            leaderboardArray.sort(function(a, b) {
                 return b[1] - a[1];
             });
 
@@ -68,7 +92,7 @@ const Socket = (function () {
             var tableBody = document.getElementById("table-body");
 
             // loop & get all data
-            leaderboardArray.forEach(function (data, index) {
+            leaderboardArray.forEach(function(data, index) {
                 var row = tableBody.insertRow(); // Create a new row
                 // Create cells and populate with data
                 var rankingCell = row.insertCell();
@@ -93,39 +117,39 @@ const Socket = (function () {
     };
 
     // This function disconnects the socket from the server
-    const disconnect = function () {
+    const disconnect = function() {
         socket.disconnect();
         socket = null;
     };
 
     // post movement to server (called by game.js)
-    const postMovement = function (movement, direction) {
-        setTimeout(function () {
+    const postMovement = function(movement, direction) {
+        setTimeout(function() {
             socket.emit("move", { playerID: _playerID, movement: movement, direction: direction });
         }, 10);
     }
 
     // post player data to server (called by ui.js)
-    const joinGame = function (playerName, playerID) {
+    const joinGame = function(playerName, playerID) {
         _playerID = playerID
         socket.emit("join game", { playerName: playerName, playerID: playerID });
     }
 
     // post the winner player name to server
-    const endGame = function (playerName) {
+    const endGame = function(playerName) {
         socket.emit("end game", playerName);
     }
 
-    const restartGame = function () {
+    const restartGame = function() {
         socket.emit("restart", true);
     }
 
     // get all players rank
-    const getLeaderboard = function () {
+    const getLeaderboard = function() {
         socket.emit("get leaderboard", true);
     }
 
-    const getCurrentPlayer = function () {
+    const getCurrentPlayer = function() {
         socket.emit("get currentPlayer", true);
     }
 
